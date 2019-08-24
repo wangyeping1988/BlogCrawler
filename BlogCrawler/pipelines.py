82d25f2bf8805e8a11b8c5b7344963121bdf8cf8
Wang Ye Ping <wangyeping1988@outlook.com>
2019-08-25 00:10:32 +0800

    request all list pages and save data to file

--- BlogCrawler/pipelines.py
+++ BlogCrawler/pipelines.py
@@ -4,8 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import codecs
+import os
 
 
 class BlogcrawlerPipeline(object):
     def process_item(self, item, spider):
+        try:
+            post_file = codecs.open('posts' + os.path.sep + item['title'] + '.md', 'wb', encoding='utf-8')
+            post_file.write(item['content'])
+            post_file.close()
+        except(IndexError, TypeError):
+            pass
+
         return item
--- BlogCrawler/spiders/cnblogs_spider.py
+++ BlogCrawler/spiders/cnblogs_spider.py
@@ -2,6 +2,7 @@
 import scrapy
 
 from BlogCrawler.items import CnblogsPostItem
+from scrapy.http import Request
 
 
 class CnblogsSpider(scrapy.Spider):
@@ -13,7 +14,32 @@ class CnblogsSpider(scrapy.Spider):
 
     # entry urls
     start_urls = [
-        'https://i.cnblogs.com'
+        'https://i.cnblogs.com/posts',
+        'https://i.cnblogs.com/posts?page=2',
+        'https://i.cnblogs.com/posts?page=3',
+        'https://i.cnblogs.com/posts?page=4',
+        'https://i.cnblogs.com/posts?page=5',
+        'https://i.cnblogs.com/posts?page=6',
+        'https://i.cnblogs.com/posts?page=7',
+        'https://i.cnblogs.com/posts?page=8',
+        'https://i.cnblogs.com/posts?page=9',
+        'https://i.cnblogs.com/posts?page=10',
+        'https://i.cnblogs.com/posts?page=11',
+        'https://i.cnblogs.com/posts?page=12',
+        'https://i.cnblogs.com/posts?page=13',
+        'https://i.cnblogs.com/posts?page=14',
+        'https://i.cnblogs.com/posts?page=15',
+        'https://i.cnblogs.com/posts?page=16',
+        'https://i.cnblogs.com/posts?page=17',
+        'https://i.cnblogs.com/posts?page=18',
+        'https://i.cnblogs.com/posts?page=19',
+        'https://i.cnblogs.com/posts?page=20',
+        'https://i.cnblogs.com/posts?page=21',
+        'https://i.cnblogs.com/posts?page=22',
+        'https://i.cnblogs.com/posts?page=23',
+        'https://i.cnblogs.com/posts?page=24',
+        'https://i.cnblogs.com/posts?page=25',
+        'https://i.cnblogs.com/posts?page=26'
     ]
 
     # def start_requests(self):
@@ -47,28 +73,38 @@ class CnblogsSpider(scrapy.Spider):
     #                   dont_filter=True)
 
     def start_requests(self):
-        yield scrapy.Request(
-            self.start_urls[0],
-            cookies={
-                '.CNBlogsCookie': '6E0D581D816B40292F2CB163F9E2F6103933AE513E86496338B4D8098A447EF63939A0742F7BE1D09364AC1F46272E1B53175A0051B2F2E21476A6051F5FDD91E83A0AED5B2DA4AE02AD96225882AAA12602975F5AB90A169A851961F9B37D620F8C4D34',
-                '.Cnblogs.AspNetCore.Cookies': 'CfDJ8BQYbW6Qx5RFuF4UTI7QvU118ut7nAbYfnRl0pAOtKCjq7-cwYn6CHWAbA4QwiOkwSvZ1jQu1PYse-z8i-D_Sl-rkl63Nvg_1w-vFssT77vMeEvCbSy2uwWYWHizRZbSazW4aoSLECfVWxioywacv3ZCzryhWkSfV2OKd-6EmsAnijvZ59eW1kYNH1keRodIMGM7k9Il2OvnOVOWSQysJbQPoYrR1uvHPY62avfQdQVhjHt7JhXhz5euovH6kVgZqFeIjUTCyqR6jc1LfS488Kr7-wVb-VMoLGYJFHKW_P2gEwzEImLJg3vYU-_Pp0NH333rhEiKaDnIHS3Y-WUpgaY9tAXSswLYTPFWCuBAe5SeY9nbRQqU9EJuix_fA0StJaNS1Nbc9MoW4LhRxctWJ_8HmnKhjuKW40d4bewALIJpJYot2X8FsS-NTr0LaPsJ4rAAS-zp4K9vlpKW_SsPe29STe_GJ0VKB0p2C18sX7WC',
-                'SERVERID': '04ead23841720026ba009cb4f597ec8c|1566653061|1566652046',
-                '__gads': 'ID=ae065093c3816478:T=1523979454:S=ALNI_MYA9AUiW8F72qKRLOTLGedFqt2nLw',
-                '_ga': 'GA1.2.1968144735.1498924730',
-                '_gid': 'GA1.2.466366568.1566609068'
-            },
-            callback=self.parse
-        )
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url,
+                cookies={
+                    '.CNBlogsCookie': '6E0D581D816B40292F2CB163F9E2F6103933AE513E86496338B4D8098A447EF63939A0742F7BE1D09364AC1F46272E1B53175A0051B2F2E21476A6051F5FDD91E83A0AED5B2DA4AE02AD96225882AAA12602975F5AB90A169A851961F9B37D620F8C4D34',
+                    '.Cnblogs.AspNetCore.Cookies': 'CfDJ8BQYbW6Qx5RFuF4UTI7QvU118ut7nAbYfnRl0pAOtKCjq7-cwYn6CHWAbA4QwiOkwSvZ1jQu1PYse-z8i-D_Sl-rkl63Nvg_1w-vFssT77vMeEvCbSy2uwWYWHizRZbSazW4aoSLECfVWxioywacv3ZCzryhWkSfV2OKd-6EmsAnijvZ59eW1kYNH1keRodIMGM7k9Il2OvnOVOWSQysJbQPoYrR1uvHPY62avfQdQVhjHt7JhXhz5euovH6kVgZqFeIjUTCyqR6jc1LfS488Kr7-wVb-VMoLGYJFHKW_P2gEwzEImLJg3vYU-_Pp0NH333rhEiKaDnIHS3Y-WUpgaY9tAXSswLYTPFWCuBAe5SeY9nbRQqU9EJuix_fA0StJaNS1Nbc9MoW4LhRxctWJ_8HmnKhjuKW40d4bewALIJpJYot2X8FsS-NTr0LaPsJ4rAAS-zp4K9vlpKW_SsPe29STe_GJ0VKB0p2C18sX7WC',
+                    'SERVERID': '04ead23841720026ba009cb4f597ec8c|1566653061|1566652046',
+                    '__gads': 'ID=ae065093c3816478:T=1523979454:S=ALNI_MYA9AUiW8F72qKRLOTLGedFqt2nLw',
+                    '_ga': 'GA1.2.1968144735.1498924730',
+                    '_gid': 'GA1.2.466366568.1566609068'
+                },
+                callback=self.parse
+            )
 
     def parse(self, response):
         """1. post list page"""
+        edit_page_url = 'https://i.cnblogs.com/EditPosts.aspx?postid={0}&update=1'
         post_list_items = response.xpath('//tr[contains(@id,"post-row-")]')
         for postListItem in post_list_items:
             try:
-                tr_id = postListItem.xpath('@href').extract_first()
+                tr_id = postListItem.xpath('@id').extract_first()
                 if tr_id:
-                    item = CnblogsPostItem()
-                    item['id'] = tr_id[9:len(tr_id)]
-                    yield item
+                    yield Request(edit_page_url.format(tr_id[9:len(tr_id)]), callback=self.parse_edit_page)
             except(IndexError, TypeError):
                 continue
+
+
+    def parse_edit_page(self, response):
+        """2. post edit page"""
+        post_title = response.xpath('//input[@id="Editor_Edit_txbTitle"]/@value').extract_first()
+        post_content = response.xpath('//textarea[@id="Editor_Edit_EditorBody"]').xpath('text()').extract_first()
+        item = CnblogsPostItem()
+        item['title'] = post_title
+        item['content'] = post_content
+        yield item
